828d961edd4eca74bc58ef327276e07d1eded82e
Lisa Figarelli <l.figarelli@jellysmack.com>
2020-11-14 15:04:20 +0100

    mise en place de firebase pour la notif push

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
 const cacheName = 'bebetopedia-1.0';
 
  
@@ -187,8 +190,26 @@ self.addEventListener("push", evt => {
     //     )
     // );
 
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    'messagingSenderId': '716668447382'
+});
 
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
 
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/itwonders-web-logo.png'
+    };
 
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
 
- 
